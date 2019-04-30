753445f7998e4ab0c1b4b166dbfabc8591459ec1
bvaagen <benvaagen24@gmail.com>
2019-04-29 20:32:37 -0700

    practicing deploying

---
+++ client/build/precache-manifest.cf26a191eb3964a464eeb736b71f4dd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed33ab0abf84623bedf8",
+    "url": "/static/css/main.ca427cff.chunk.css"
+  },
+  {
+    "revision": "ed33ab0abf84623bedf8",
+    "url": "/static/js/main.5affd04d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26553e7036fb655a5e00",
+    "url": "/static/js/2.f43e1651.chunk.js"
+  },
+  {
+    "revision": "a277ca3e51c6f1ce91919af0eaa27b89",
+    "url": "/index.html"
+  }
+];
